72cdb5219e95c6ad2bfaa00efeefd6db9f3c20c0
Albino Pereira Lima <albino.lima@easynvest.com.br>
2020-11-02 13:15:50 -0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,55 @@
 <template>
-  <div id="app">
-    <header>
-      <h1>Vue.js SPA</h1>
-    </header>
-    <main>
-      <aside class="sidebar">
-      </aside>
-      <div class="content">
-      </div>
-    </main>
-  </div>
+    <div>
+        <h3 class="text-center">Calcular Investimento</h3>
+        <div class="row">
+            <div class="col-md-6">
+                <form @submit.prevent="calculate">
+                    <div class="form-group">
+                        <label>Data Inicial</label>
+                        <input type="date" required class="form-control" min="2010-01-04" max="2019-12-03" v-model="parameter.investmentDate">
+                    </div>
+                    <div class="form-group">
+                        <label>Data Final</label>
+                        <input type="date" required class="form-control" min="2010-01-04" max="2019-12-03" v-model="parameter.currentDate">
+                    </div>
+                    <div class="form-group">
+                        <label>Percentual</label>
+                        <input type="number" required class="form-control" min="0" step=".01" v-model="parameter.cdbRate">
+                    </div>
+                    <div class="form-group">
+                        <label>Valor</label>
+                        <input type="number" required class="form-control" min="0" step=".01" v-model="parameter.investmentValue">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Calcular</button>
+                </form>
+            </div>
+        </div>
+        <div class="row">
+          {{unitPrices}}
+        </div>
+    </div>
 </template>
 
 <script>
-  export default{
-    data(){
-      return{
-        posts:[]
-      }
+    export default {
+        data() {
+            return {
+              parameter: {},
+              unitPrices: null  
+            }
+        },
+        methods: {
+            calculate() {
+
+                this.axios
+                    .post('http://localhost:3000/calculator', this.parameter)
+                    .then(response => (
+                        //this.$router.push({name: 'home'})
+                        this.unitPrices = response.data
+                    ))
+                    .catch(error => console.log(error))
+                    .finally(() => this.loading = false)
+            }
+        }
     }
-  }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+import App from './App.vue';
 import Vue from 'vue'
-import App from './App.vue'
+import axios from 'axios';
+
+Vue.prototype.axios = axios;
 
 new Vue({
   el: '#app',
-  render: h => h(App)
-})
+  render: h => h(App),
+});
